7a016c57934fda64f246a5438c1bf263b1f400ed
Ben Mulford <39347820+thedevbc@users.noreply.github.com>
2021-08-06 16:10:45 -0500

    updates

---
+++ MagicEightBall/MagicEightBall.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using System.Diagnostics;
+
+namespace MagicEightBall
+{
+    public partial class MagicEightBall : ContentPage
+    {
+        public Responses responses { get; set; }
+
+        public MagicEightBall()
+        {
+            InitializeComponent();
+            responses = new Responses();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Accelerometer.ShakeDetected += Accelerometer_ShakeDetected;
+            Accelerometer.Start(SensorSpeed.Game);
+        }
+
+        protected override void OnDisappearing()
+        {
+            Accelerometer.ShakeDetected -= Accelerometer_ShakeDetected;
+            Accelerometer.Stop();
+            base.OnDisappearing();
+        }
+
+        private void Accelerometer_ShakeDetected(object sender, EventArgs e)
+        {
+            if (responses == null)
+            {
+                responses = new Responses();
+            }
+
+            responses.CurrentResponse = responses.GetRandomResponse();
+
+            // prevent same response twice in a row
+            if (responses.CurrentResponse == responseDisplay.Text)
+            {
+                responses.CurrentResponse = responses.GetRandomResponse();
+            }
+
+            responseDisplay.Text = responses.CurrentResponse;
+        }
+
+        private async void backBtn_Pressed(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ MagicEightBall/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace MagicEightBall
+{
+    public partial class MainPage : ContentPage
+    {
+        public Responses responses { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            responses = new Responses();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Accelerometer.ShakeDetected += Accelerometer_ShakeDetected;
+            Accelerometer.Start(SensorSpeed.Game);
+        }
+
+        protected override void OnDisappearing()
+        {
+            Accelerometer.ShakeDetected -= Accelerometer_ShakeDetected;
+            Accelerometer.Stop();
+            base.OnDisappearing();
+        }
+
+        private async void Accelerometer_ShakeDetected(object sender, EventArgs e)
+        {
+            await responseDisplay.FadeTo(0, 500);
+            if (responses == null)
+            {
+                responses = new Responses();
+            }
+
+            responses.CurrentResponse = responses.GetRandomResponse();
+
+            // prevent same response twice in a row
+            if (responses.CurrentResponse == responseDisplay.Text)
+            {
+                responses.CurrentResponse = responses.GetRandomResponse();
+            }
+            
+            responseDisplay.Text = responses.CurrentResponse;
+            await responseDisplay.FadeTo(1, 1500);
+        }
+
+        //private async void StartBtn_Pressed(object sender, EventArgs e)
+        //{
+        //    await Navigation.PushAsync(new MagicEightBall());
+        //}
+
+    }
+}
